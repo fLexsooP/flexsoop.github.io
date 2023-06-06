0082874faa546b5c17e48a91918a54ef278bfb95
Chence Shi <ascc594557067@gmail.com>
2023-06-05 21:33:05 -0700

    decorate resume download button

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,31 @@
-import PortfolioBlock from "./PortfolioBlock";
-import {Box, Grid} from "@mui/material";
-import {info} from "../../info/Info";
+import PortfolioBlock from './PortfolioBlock';
+import { Box, Grid } from '@mui/material';
+import { info } from '../../info/Info';
 
 export default function Portfolio() {
-    return (
-        <Box mx={20} mt={10}>
-            <Grid container display={'flex'} justifyContent={'center'}>
-                {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
-                       <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
-                   </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box
+      mx={20}
+      mt={10}>
+      <Grid
+        container
+        display={'flex'}
+        justifyContent={'center'}>
+        {info.portfolio.map((project, index) => (
+          <Grid
+            item
+            xs={12}
+            md={6}
+            key={index}>
+            <PortfolioBlock
+              image={project.image}
+              live={project.live}
+              source={project.source}
+              title={project.title}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -3,29 +3,32 @@ import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
 import Chenceshi_Resume from '../../info/Chenceshi_Resume.pdf';
 
 export default function Resume() {
-
   return (
-
     <Box
       display={'flex'}
       flexDirection={'column'}
       alignItems={'center'}
       mt={'3rem'}>
-        <Box
-          p={1.5}
-          border={'2px solid black'}
-          borderRadius={'25px'}
-          mb='2rem'>
-          <a
-            onClick={() => window.open(Chenceshi_Resume)}
-            target={'_blank'}
-            rel="noopener noreferrer">
-            Download
-          </a>
-        </Box>
-        <Document file={Chenceshi_Resume}>
-          <Page pageNumber={1} renderTextLayer={false} scale='1.5'/>
-        </Document>
+      <Box
+        p={1.5}
+        border={'2px solid black'}
+        borderRadius={'25px'}
+        mb="2rem">
+        <div
+          style={{ cursor: 'pointer' }}
+          onClick={() => window.open(Chenceshi_Resume)}
+          target={'_blank'}
+          rel="noopener noreferrer">
+          Download
+        </div>
+      </Box>
+      <Document file={Chenceshi_Resume}>
+        <Page
+          pageNumber={1}
+          renderTextLayer={false}
+          scale="1.5"
+        />
+      </Document>
     </Box>
   );
 }
