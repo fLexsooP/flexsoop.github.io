0a2ad028fccd5ae6b12cc52ab8ea7764958bf536
Chence Shi <ascc594557067@gmail.com>
2023-06-05 17:21:42 -0700

    config deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/portfolio/',
+// });
+
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [
+      react(),
+    ],
+  });
+};
